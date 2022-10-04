eac3cea478472c83c0cbfe9e227d2fafbd08e417
mike doornenbal <mikedoornenbal9@icloud.com>
2022-10-04 11:31:03 +0100

    database test

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 const sensor = require('ds18b20-raspi');
 const tempC = sensor.readSimpleC();
-console.log(`${tempC} degC`);
+// console.log(`${tempC} degC`);
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//setup database connection
+const mysql = require('mysql');
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "iot-project",
+  });
+
+  con.connect(function(error){
+    if(error) throw error;
+    console.log("Connected!");
+  })
+
+
+app.post('/currentTemp', (req, res)=>{
+        var sql = `INSERT INTO temperature (temp) VALUES (${tempC})`;
+        con.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("1 record inserted: id-" + result[1]);
+          
+        
+      });
+    res.status(200).send({"tempNow": 26});
+    res.end();
+});
+
+app.listen(port, console.log('server is running!!!'));
