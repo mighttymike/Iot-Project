560cf08a9d37ade4693dcd2ce10bd37d0ee48cac
mike doornenbal <mikedoornenbal9@icloud.com>
2022-10-04 11:42:08 +0100

    bug fix

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 3000;
 //setup database connection
 const mysql = require('mysql');
 var con = mysql.createConnection({
-    host: "172.20.10.3:3306",
+    host: "172.20.10.3",
     user: "root",
     password: "",
     database: "iot-project",
